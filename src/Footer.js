3a0eb2f8e252fbcf8e0fffc079ef907a2a587135
Mhelamolc <142678129+Mhelamolc@users.noreply.github.com>
2023-11-15 00:57:39 -0800

    1st commit

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+       <p>&copy;Copyright 2023 E. Cloma Correlation 1 Project</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/MainContent.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main>
+       <img src="/images/me.png" alt="Logo" style={{ width: '100px', height: 'auto' }} />
+      <p> Mhel here...Born and raised in the Philippines. A graduate of Bachelor of Science in Computer Science. I'm a self-taught graphic artist and I'm a Youtube guy...meaning I learned more of my skills online and got into a Interactive Multimedia Certificate Program (IMCP) at San Diego Continuing Education.</p>
+      <p>Hoping that i can use my knowledge in graphic design into building a good webpages.</p>
+      <p>I loved the process of tshirt printing and that's the reasons why i got myself into Graphic Design. Now I print my own shirts and I have my online store as well.
+      </p>
+      <h2>Things I love to do:</h2>
+      <img src="/images/10-.jpg" alt="tshirt" style={{ width: '400px', height: 'auto' }} />
+      <p>Printing</p>
+      <img src="/images/9-.jpg" alt="band" style={{ width: '400px', height: 'auto' }} />
+      <p>Jamming</p>
+      <img src="/images/3-.png" alt="design" style={{ width: '400px', height: 'auto' }} />
+      <p>Designing</p>
+    </main>
+  );
+}
+
+export default MainContent;
